0b46d32f38c42122bb8b64b643983d43a8271f77
Miracle Pule <ggh9947@autuni.ac.nz>
2021-05-25 12:06:52 +1200

    rename headings

--- articalSubmitter/index.php
+++ articalSubmitter/index.php
@@ -7,16 +7,16 @@
             <div class="container">
                 <div class="row text-white bg-light border-rounded">
                     <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
-                        <h1 class="font-weight-normal py-2 text-truncate text-custom text-left">Artical Submitter
+                        <h1 class="font-weight-normal py-2 text-truncate text-custom text-left">Article Submitter
                             Home Page</h1>
 
                         <div class="px-2">
                             <div class="row">
                                 <div class="col-12">
-                                    <a class="nav-link btn btn-primary m-4" href="add.php">Add Artical</a>
+                                    <a class="nav-link btn btn-primary m-4" href="add.php">Add Article</a>
                                 </div>
                                 <div class="col-12">
-                                    <a class="nav-link btn btn-primary m-4" href="analyze.php">Analyze Artical</a>
+                                    <a class="nav-link btn btn-primary m-4" href="analyze.php">Analyze Article</a>
                                 </div>
                             </div>
                         </div>
