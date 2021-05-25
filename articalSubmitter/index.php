b0b20ddce15b2e7cce4a1a1fa8e92d4ad493bdbe
Miracle Pule <ggh9947@autuni.ac.nz>
2021-05-26 11:16:21 +1200

    update index.php

--- articalSubmitter/index.php
+++ articalSubmitter/index.php
@@ -7,16 +7,19 @@
             <div class="container">
                 <div class="row text-white bg-light border-rounded">
                     <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
-                        <h1 class="font-weight-normal py-2 text-truncate text-custom text-left">Article Submitter
+                        <h1 class="font-weight-normal py-2 text-truncate text-custom text-left">Artical Submitter
                             Home Page</h1>
 
                         <div class="px-2">
                             <div class="row">
                                 <div class="col-12">
-                                    <a class="nav-link btn btn-primary m-4" href="add.php">Add Article</a>
+                                    <a class="nav-link btn btn-primary m-4" href="add.php">Add Artical</a>
                                 </div>
                                 <div class="col-12">
-                                    <a class="nav-link btn btn-primary m-4" href="analyze.php">Analyze Article</a>
+                                    <a class="nav-link btn btn-primary m-4" href="analyze.php">Analyze Artical</a>
+                                </div>
+                                <div class="col-12">
+                                    <a class="nav-link btn btn-primary m-4" href="import.php">Import Artical</a>
                                 </div>
                             </div>
                         </div>
